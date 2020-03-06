01aa5947d2239bdd58c6f208f1e25d1bf862aa87
Matthijs van Spelde <I404906@fhict.nl>
2020-03-06 14:35:38 +0100

    Initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currentHealth;
+    public Healthbar healthbar;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthbar.SetMaxHealth(maxHealth);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TakeDamage(5);
+        }
+    }
+
+    public void TakeDamage(int damage) 
+    {
+        currentHealth -= damage;
+        healthbar.SetHealth(currentHealth);
+    }
+}
---
+++ Assets/Scripts/Trap.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trap : MonoBehaviour
+{
+    public int hitPoints = 10;
+    public AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource.GetComponent<AudioSource>();
+    }
+
+    private void OnCollisionEnter(UnityEngine.Collision collision)
+    {
+        if (collision.collider.name == "Player")
+        {
+            Debug.Log("Hit trap");
+            collision.gameObject.GetComponent<Player>().TakeDamage(hitPoints);
+            audioSource.Play();
+        }
+
+    }
+}
